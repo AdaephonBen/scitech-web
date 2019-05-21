997c8adaad49a31c2e4ecee4f294f54a33f6cd5b
adaephonben <ma18btech11011@iith.ac.in>
2019-05-21 09:10:04 +0530

    Navbar issue fixed

--- scitechwebsite/mainscitech.js
+++ scitechwebsite/mainscitech.js
@@ -1,6 +1,7 @@
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
+var prevNav = null;
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
@@ -13,5 +14,20 @@ function myFunction() {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+  var allNavs = document.getElementsByClassName("navbarControl");
+  for (var i = 0 ; i < allNavs.length ; i++)
+  {
+    var bounding = allNavs[i].getBoundingClientRect();
+    var x = "navbar-" + (i+1);
+    var toBeHighlighted = document.getElementById(x);
+
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    {
+      if (prevNav)
+        prevNav.classList.remove("highlighted");
+      toBeHighlighted.classList.add("highlighted");
+      prevNav = toBeHighlighted ;
+    }
   }
 }
