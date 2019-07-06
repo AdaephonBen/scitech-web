5f7da91b025cf00a3d9854f98c0218a112a28df3
adaephonben <ma18btech11011@iith.ac.in>
2019-07-06 14:19:00 +0530

    PICTURE CHANGING THING

--- scitechwebsite/mainscitech.js
+++ scitechwebsite/mainscitech.js
@@ -1,6 +1,21 @@
 // When the user scrolls the page, execute myFunction 
+window.allPhotos = document.getElementsByClassName("all-photos");
+window.allSortButtons = document.getElementsByClassName("sort-button");
 window.onscroll = function() {myFunction()};
-
+var onSortButtonClick = (event) => {
+  let sortCategory = event.currentTarget.id.substring(5);
+  for (let whatever = 0 ; whatever < window.allPhotos.length ; whatever++)
+  {
+    let current = window.allPhotos[whatever]; 
+    if (current.classList.contains(sortCategory))
+    {
+      current.style.display = "block";
+    }
+    else {
+      current.style.display = "none";
+    }
+  }
+};
 var prevNav = null;
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -20,14 +35,23 @@ function myFunction() {
   {
     var bounding = allNavs[i].getBoundingClientRect();
     var x = "navbar-" + (i+1);
-    var toBeHighlighted = document.getElementById(x);
+    var toBehighlighted = document.getElementById(x);
 
     if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     {
       if (prevNav)
         prevNav.classList.remove("highlighted");
-      toBeHighlighted.classList.add("highlighted");
-      prevNav = toBeHighlighted ;
+      toBehighlighted.classList.add("highlighted");
+      prevNav = toBehighlighted ;
     }
   }
 }
+
+window.onload = () => {
+  
+  
+  for (let i = 0 ; i < window.allSortButtons.length ; i++)
+  {
+    window.allSortButtons[i].addEventListener("click", onSortButtonClick);
+  }
+};
